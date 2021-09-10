0e3960dde2b8ce4482cc72d6de74b85aa2cd9aba
Pranta07 <palpranta@gmail.com>
2021-09-10 15:19:38 +0600

    arrow-function TS

---
+++ arrow-function.ts
@@ -0,0 +1,6 @@
+const sub = (num1: number, num2: number): number => {
+    return num1 - num2;
+};
+
+const res: number = sub(10, 4);
+console.log(res);
---
+++ js/arrow-function.js
@@ -0,0 +1,6 @@
+"use strict";
+const sub = (num1, num2) => {
+    return num1 - num2;
+};
+const res = sub(10, 4);
+console.log(res);
